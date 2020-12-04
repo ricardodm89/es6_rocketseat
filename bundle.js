3e5c4deae843594f62475ca9380170adb3784c95
ricardodm89 <ricado89@gmailcom>
2020-12-04 09:19:53 -0300

    add plugin rest-spread

--- bundle.js
+++ bundle.js
@@ -6,20 +6,26 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-alert("teste");
+var TodoList = /*#__PURE__*/function () {
+  function TodoList() {
+    _classCallCheck(this, TodoList);
 
-var Teste = /*#__PURE__*/function () {
-  function Teste() {
-    _classCallCheck(this, Teste);
+    this.todos = [];
   }
 
-  _createClass(Teste, [{
-    key: "metodo",
-    value: function metodo() {}
-  }, {
-    key: "outro",
-    value: function outro() {}
+  _createClass(TodoList, [{
+    key: "addTodo",
+    value: function addTodo() {
+      this.todos.push('Novo todo');
+      console.log(this.todos);
+    }
   }]);
 
-  return Teste;
+  return TodoList;
 }();
+
+var MinhaLista = new TodoList();
+
+document.getElementById("novoTodo").onclick = function () {
+  MinhaLista.addTodo();
+};
--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-alert("teste");
-
-class Teste {
-    metodo(){
+class TodoList{
+    constructor(){
+        this.todos = []
 
     }
 
-    outro(){
-        
+    addTodo(){
+        this.todos.push('Novo todo');
+        console.log(this.todos);
     }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById("novoTodo").onclick = function(){
+    MinhaLista.addTodo();
 }
