078260c50ab8a2f5931f87fe688586cd4e3993c3
ricardodm89 <ricado89@gmailcom>
2020-12-07 19:10:37 -0300

    Desafio Modulo 01 - exercício 3.2

--- desafios/exe03.js
+++ desafios/exe03.js
@@ -1,6 +1,16 @@
 // 3.1
 const arr = [1, 2, 3, 4, 5];
-arr.map(item => item + 10);
-console.log(arr);
+const soma = arr.map(item => item + 10);
+console.log(soma);
 
 //3.2
+// Dica: Utilize uma constante pra function
+const usuario = { nome: 'Diego', idade: 23 };
+
+const mostraIdade = (usuario => usuario.idade);
+
+console.log(mostraIdade(usuario));
+
+
+
+
