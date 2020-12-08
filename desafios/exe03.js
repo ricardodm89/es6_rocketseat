e58b64db7c98339bcdacb6da9914385bbcb0daad
ricardodm89 <ricado89@gmailcom>
2020-12-08 12:06:13 -0300

    Desafio Modulo 01 - exercício 4.1

--- desafios/exe03.js
+++ desafios/exe03.js
@@ -1,16 +1,32 @@
 // 3.1
 const arr = [1, 2, 3, 4, 5];
 const soma = arr.map(item => item + 10);
-console.log(soma);
+console.log('Exercício 3.1',soma);
+
 
 //3.2
 // Dica: Utilize uma constante pra function
 const usuario = { nome: 'Diego', idade: 23 };
 
-const mostraIdade = (usuario => usuario.idade);
+const mostraIdade = usuario => usuario.idade;
 
-console.log(mostraIdade(usuario));
+console.log('Exercício 3.2', mostraIdade(usuario));
 
 
+// 3.3
+// Dica: Utilize uma constante pra function
+const nome = "Diego";
+const idade = 23;
 
+const mostraUsuario = (nome = 'Diego', idade = 18) => ({nome, idade});
+
+console.log('Exercício 3.3',mostraUsuario(nome, idade));
+console.log('Exercício 3.3',mostraUsuario(nome));
+
+// 3.4
+const promise = function() {
+    return new Promise(function(resolve, reject) {
+    return resolve();
+    })
+}
 
---
+++ desafios/exe04.js
@@ -0,0 +1,18 @@
+//4.1 Desestruturação simples
+
+const empresa = {
+    nome: 'Rocketseat',
+    endereco: {
+    cidade: 'Rio do Sul',
+    estado: 'SC',
+    }
+   };
+  
+
+   const {nome, endereco: {cidade, estado}} = empresa;
+
+
+
+console.log(nome); // Rocketseat
+console.log(cidade); // Rio do Sul
+console.log(estado); // SC
