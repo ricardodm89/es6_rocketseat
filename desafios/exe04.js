73b53cc02eccb4df5ed648ed142984b45063900d
ricardodm89 <ricado89@gmailcom>
2020-12-08 12:31:20 -0300

    Desafio Modulo 01 - exercício 4.2

--- desafios/exe04.js
+++ desafios/exe04.js
@@ -7,12 +7,23 @@ const empresa = {
     estado: 'SC',
     }
    };
-  
-
-   const {nome, endereco: {cidade, estado}} = empresa;
 
 
+const {nome, endereco: {cidade, estado}} = empresa;
 
 console.log(nome); // Rocketseat
 console.log(cidade); // Rio do Sul
 console.log(estado); // SC
+
+
+//4.2 Desestruturação em parâmetros
+
+
+function mostraInfo({nome, idade}) {
+    //return `${usuario.nome} tem ${usuario.idade} anos.`;
+    return info = `${nome} tem ${idade} anos.`;
+   }
+
+   mostraInfo({ nome: 'Diego', idade: 23 })
+   console.log(info);
+    
