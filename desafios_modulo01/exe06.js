8e3c33523f0f4fa561996bb0557d0fef06fe181b
ricardodm89 <ricado89@gmailcom>
2020-12-10 13:50:23 -0300

    Desafio Modulo 01 - exercício 6

---
+++ desafios_modulo01/exe06.js
@@ -0,0 +1,11 @@
+// 6 - Converta o seguinte trecho de código utilizando Template Literals:
+
+const usuario = 'Diego';
+const idade = 23;
+
+//console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
+
+// Com Template Literals:
+console.log(`O usuário ${usuario} possui ${idade} anos.`);
+
+
