4b53d03ef4b1c966e190c1cac413da35f3f594a0
ricardodm89 <ricado89@gmailcom>
2020-12-10 12:55:53 -0300

    Desafio Modulo 01 - exercício 5.2

--- desafios_modulo01/exe05.js
+++ desafios_modulo01/exe05.js
@@ -15,3 +15,26 @@ console.log(soma(1, 2, 3, 4, 5, 6));
 
 //console.log(soma(1, 2, 3, 4, 5, 6)); // 21
 //console.log(soma(1, 2)); // 3
+
+
+//5.2 spread
+//Crie uma variável usuario2 que contenha todos os dados do usuário porém com nome Gabriel.
+//Crie uma variável usuario3 que contenha todos os dados do usuário porém com cidade Lontras.
+
+
+const usuario = {
+    nome: 'Diego',
+    idade: 23,
+    endereco: {
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil',
+    }
+   };
+
+   const usuario2 = {...usuario, nome:'Gabriel'};
+   console.log('Exercício 5.1', usuario2)
+
+   const usuario3 = {...usuario, endereco:{cidade:'Lontras'}};
+   console.log('Exercício 5.2', usuario3)
+   
