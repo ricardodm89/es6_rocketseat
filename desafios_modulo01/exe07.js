ef982414c5958c899aad2960fb216f33811cac1d
ricardodm89 <ricado89@gmailcom>
2020-12-10 13:56:10 -0300

    Desafio Modulo 01 - exercício 7

---
+++ desafios_modulo01/exe07.js
@@ -0,0 +1,15 @@
+// 7º exercício
+//Utilize a sintaxe curta de objetos (Object Short Syntax) no seguinte objeto:
+
+const nome = 'Diego';
+
+const idade = 23;
+
+const usuario = {
+ nome,
+ idade,
+ cidade: 'Rio do Sul',
+};
+
+
+console.log('Exercício 7',usuario)
